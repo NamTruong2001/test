56a89b52a4f21e91d6c15c95320d24d0d23bc002
Nguyễn Nam Trường [FLVN] <truong_nn@flinters.vn>
2024-09-12 23:55:03 +0700

    mod new.py

--- new.py
+++ new.py
@@ -1,1 +1,7 @@
+import requests
+
+
 print("hello world")
+response = requests.get(url="https://vi.wikipedia.org/wiki/S%E1%BB%B1_ki%E1%BB%87n_b%C3%A1n_non_c%E1%BB%95_phi%E1%BA%BFu_GameStop")
+with open('response.html', "w") as f:
+    f.write(response.text)
